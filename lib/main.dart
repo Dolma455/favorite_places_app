926ff986efbf1bf3ff13553a2754b8ac9fe4fd8a
Dolma <dolma1918@gmail.com>
2024-01-09 20:42:14 +0545

    feat: Riverpod and Provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:favorite_places_app/app/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 final colorScheme = ColorScheme.fromSeed(
@@ -20,5 +21,5 @@ final theme = ThemeData().copyWith(
         titleLarge: GoogleFonts.ubuntuCondensed(fontWeight: FontWeight.bold)));
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -24,6 +24,7 @@ class PlacesList extends StatelessWidget {
                       color: Theme.of(context).colorScheme.onBackground,
                     ),
               ),
-            )));
+            ))
+            );
   }
 }
